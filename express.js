70fa5b38f6c2d891c5f7a2d53b9e1066a71dce8f
Ates Goral <ates@magnetiq.com>
2014-08-06 01:27:28 -0400

    Fallback to static server and disable caching

--- express.js
+++ express.js
@@ -1,12 +1,21 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
     morgan = require('morgan'),
     demock = require('demock-express');
 
 var app = express();
 
-var logFmt = ':method :url :status :response-time';
+var staticOptions = {
+    redirect: false,
+    setHeaders: function (res) {
+        res.setHeader('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    }
+};
 
-app.use(morgan(logFmt));
-app.use(demock());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan(':method :url', { immediate: true }));
+app.use(morgan('--> :method :url :status :response-time', { immediate: false }));
+app.use(demock('/api', __dirname, staticOptions));
+app.use(express.static(__dirname, staticOptions));
 
 app.listen(3000);
